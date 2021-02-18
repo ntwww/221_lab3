f2914474fd482d2948a1db72c52b052b1df57c27
ntwww <nattawut.rod@ku.th>
2021-02-18 16:43:15 +0700

    Factorization

--- 6320502398_3_3.c
+++ 6320502398_3_3.c
@@ -1,28 +1,24 @@
-#include<Stdio.h>
+#include<stdio.h>
 int main()
 {
-    int n,a=2;
+    int a=2,n,x=0;
     scanf("%d",&n);
     if(n==1)
-        printf("%d\n%d",n,0);
-    else
     {
-
-        while (1)
+        printf("%d\n",n);
+    }
+    while(n!=1)
+    {
+        if(n%a==0)
         {
-            if(n%a==0)
+            while(n%a==0)
             {
                 printf("%d\n",a);
                 n/=a;
             }
-            else
-                a++;
-            if (n==1)
-            {
-             printf("0");
-                break;
-            }
         }
-
+        a++;
     }
+    printf("%d",x);
+    return 0;
 }
