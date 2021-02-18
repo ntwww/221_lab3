801056cb6329f2ca6552235e995ec3d02e5a501b
ntwww <nattawut.rod@ku.th>
2021-02-18 17:17:22 +0700

    prime friend2

--- 6320502398_3_4.c
+++ 6320502398_3_4.c
@@ -3,8 +3,9 @@ int main()
 {
     int n;
     scanf("%d" ,&n);
-    if(n==1)
-        printf("1");
+    if(n==1||n==0)
+
+        printf("%d",n);
     while(n>1)
     {
         if((n%2==0||n%3==0||n%5==0||n%7==0)&&(n!=2&&n!=3&&n!=5&&n!=7))
