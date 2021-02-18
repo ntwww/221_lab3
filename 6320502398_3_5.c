578053d115378cfbc69ecffc74e6edeb8da93d97
ntwww <nattawut.rod@ku.th>
2021-02-18 17:21:42 +0700

    Lucky Number

---
+++ 6320502398_3_5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main ()
+{
+    int n,x,i,s=0;
+    scanf ("%d",&n);
+    x=n;
+    for (i=2; ;i++)
+    {
+        if (x%i==0)
+        {
+            s++;
+        }
+        else
+        {
+            break;
+        }
+
+    }
+    if (s!=3)
+    {
+            printf ("%d is not a Lucky Number.",n);
+    }
+        else
+    {
+             printf ("%d is a Lucky Number",n);
+    }
+    return 0;
+}
